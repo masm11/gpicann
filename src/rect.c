a5afd15424a6b8c13d5ddbffabd7a4cb7ebb03cb
Yuuki Harano <masm+github@masm11.me>
2020-02-24 23:24:46 +0900

    drag handle.

--- src/rect.c
+++ src/rect.c
@@ -4,8 +4,71 @@
 #include "common.h"
 #include "shapes.h"
 
+enum {
+    HANDLE_TOP_LEFT,
+    HANDLE_TOP,
+    HANDLE_TOP_RIGHT,
+    HANDLE_LEFT,
+    HANDLE_RIGHT,
+    HANDLE_BOTTOM_LEFT,
+    HANDLE_BOTTOM,
+    HANDLE_BOTTOM_RIGHT,
+
+    HANDLE_NR
+};
+
 static int beg_x = 0, beg_y = 0;
-static int orig_x = 0, orig_y = 0;
+static int orig_x = 0, orig_y = 0, orig_w = 0, orig_h = 0;
+static int dragging_handle = -1;
+
+struct handle_geom_t {
+    int x, y, width, height;
+};
+
+static void make_handle_geoms(struct parts_t *p, struct handle_geom_t *bufp)
+{
+    struct handle_geom_t *bp = bufp;
+    
+    bp->x = p->x;
+    bp->y = p->y;
+    bp++;
+    
+    bp->x = p->x + p->width / 2;
+    bp->y = p->y;
+    bp++;
+    
+    bp->x = p->x + p->width;
+    bp->y = p->y;
+    bp++;
+    
+    bp->x = p->x;
+    bp->y = p->y + p->height / 2;
+    bp++;
+    
+    bp->x = p->x + p->width;
+    bp->y = p->y + p->height / 2;
+    bp++;
+    
+    bp->x = p->x;
+    bp->y = p->y + p->height;
+    bp++;
+    
+    bp->x = p->x + p->width / 2;
+    bp->y = p->y + p->height;
+    bp++;
+    
+    bp->x = p->x + p->width;
+    bp->y = p->y + p->height;
+    bp++;
+    
+    for (int i = 0; i < HANDLE_NR; i++) {
+	bp = bufp + i;
+	bp->x -= 4;
+	bp->y -= 4;
+	bp->width = 8;
+	bp->height = 8;
+    }
+}
 
 void rect_draw(struct parts_t *parts, GtkWidget *drawable, cairo_t *cr)
 {
@@ -16,20 +79,13 @@ void rect_draw(struct parts_t *parts, GtkWidget *drawable, cairo_t *cr)
 
 void rect_draw_handle(struct parts_t *parts, GtkWidget *drawable, cairo_t *cr)
 {
-    struct {
-	int x;
-	int y;
-    } poses[] = {
-	{ parts->x,                parts->y },
-	{ parts->x + parts->width, parts->y },
-	{ parts->x,                parts->y + parts->height },
-	{ parts->x + parts->width, parts->y + parts->height },
-    };
+    struct handle_geom_t handles[HANDLE_NR];
+    make_handle_geoms(parts, handles);
     
     cairo_save(cr);
     cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 1.0);
-    for (int i = 0; i < 4; i++) {
-	cairo_rectangle(cr, poses[i].x - 5, poses[i].y - 5, 10, 10);
+    for (int i = 0; i < 8; i++) {
+	cairo_rectangle(cr, handles[i].x, handles[i].y, handles[i].width, handles[i].height);
 	cairo_stroke(cr);
     }
     cairo_restore(cr);
@@ -37,6 +93,25 @@ void rect_draw_handle(struct parts_t *parts, GtkWidget *drawable, cairo_t *cr)
 
 gboolean rect_select(struct parts_t *parts, int x, int y, gboolean selected)
 {
+    struct handle_geom_t handles[HANDLE_NR];
+    make_handle_geoms(parts, handles);
+    
+    if (selected) {
+	for (int i = 0; i < HANDLE_NR; i++) {
+	    if (x >= handles[i].x && x < handles[i].x + handles[i].width
+		    && y >= handles[i].y && y < handles[i].y + handles[i].height) {
+		beg_x = x;
+		beg_y = y;
+		orig_x = parts->x;
+		orig_y = parts->y;
+		orig_w = parts->width;
+		orig_h = parts->height;
+		dragging_handle = i;
+		return TRUE;
+	    }
+	}
+    }
+    
     int x1, x2, y1, y2, t;
     
     x1 = parts->x;
@@ -59,20 +134,54 @@ gboolean rect_select(struct parts_t *parts, int x, int y, gboolean selected)
     beg_y = y;
     orig_x = parts->x;
     orig_y = parts->y;
+    dragging_handle = -1;
     
     return x >= x1 && x < x2 && y >= y1 && y < y2;
 }
 
-void rect_drag_step(struct parts_t *parts, int x, int y)
+void rect_drag_step(struct parts_t *p, int x, int y)
 {
-    parts->x = orig_x + (x - beg_x);
-    parts->y = orig_y + (y - beg_y);
-    printf("step\n");
+    int dx = x - beg_x;
+    int dy = y - beg_y;
+    
+    switch (dragging_handle) {
+    case HANDLE_TOP_LEFT:
+    case HANDLE_LEFT:
+    case HANDLE_BOTTOM_LEFT:
+	p->x = orig_x + dx;
+	p->width = orig_w - dx;
+    }
+    
+    switch (dragging_handle) {
+    case HANDLE_TOP_RIGHT:
+    case HANDLE_RIGHT:
+    case HANDLE_BOTTOM_RIGHT:
+	p->width = orig_w + dx;
+    }
+
+    switch (dragging_handle) {
+    case HANDLE_TOP_LEFT:
+    case HANDLE_TOP:
+    case HANDLE_TOP_RIGHT:
+	p->y = orig_y + dy;
+	p->height = orig_h - dy;
+    }
+
+    switch (dragging_handle) {
+    case HANDLE_BOTTOM_LEFT:
+    case HANDLE_BOTTOM:
+    case HANDLE_BOTTOM_RIGHT:
+	p->height = orig_h + dy;
+    }
+    
+    if (dragging_handle == -1) {
+	p->x = orig_x + dx;
+	p->y = orig_y + dy;
+    }
 }
 
 void rect_drag_fini(struct parts_t *parts, int x, int y)
 {
-    printf("fini\n");
 }
 
 struct parts_t *rect_create(int x, int y)
