6b5064a155cb9eab118335391ad2a861b1b6998d
Yuuki Harano <masm+github@masm11.me>
2020-03-05 21:52:47 +0900

    reshape icons.inc.

--- src/genicontable.sh
+++ src/genicontable.sh
@@ -3,11 +3,13 @@
 set -e
 
 for icon in $*; do
-    cat $icon.inc
+    label=$(echo ${icon} | sed -e 's|-|_|g')
+    sed -e 's|"|\\"|g' -e "s|^|static const char ${label}_svg[] = \"|" -e 's|$|";|' < ../icons/${icon}.svg
+    echo ''
+    echo ''
 done
 
 cat <<EOF
-
 
 static struct {
   const char *filename;
