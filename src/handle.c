54ea2259b9391fb09bfd6dddb7f2af2c3fff196a
Yuuki Harano <masm+github@masm11.me>
2020-03-08 18:17:24 +0900

    change handles.

--- src/handle.c
+++ src/handle.c
@@ -6,14 +6,18 @@
 void handle_draw(struct handle_t *handles, int nr, cairo_t *cr)
 {
     cairo_save(cr);
+    cairo_set_line_width(cr, 1);
     cairo_set_source_rgba(cr, 1, 1, 1, 1);
+    cairo_set_antialias(cr, CAIRO_ANTIALIAS_NONE);
     for (int i = 0; i < nr; i++) {
 	cairo_rectangle(cr, handles[i].x, handles[i].y, handles[i].width, handles[i].height);
 	cairo_stroke(cr);
     }
     cairo_set_source_rgba(cr, 0, 0, 0, 1);
+    double dashes = 1;
+    cairo_set_dash(cr, &dashes, 1, 0);
     for (int i = 0; i < nr; i++) {
-	cairo_rectangle(cr, handles[i].x+1, handles[i].y+1, handles[i].width-2, handles[i].height-2);
+	cairo_rectangle(cr, handles[i].x, handles[i].y, handles[i].width, handles[i].height);
 	cairo_stroke(cr);
     }
     cairo_restore(cr);
